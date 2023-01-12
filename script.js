c5f30d48cfa3483a232ea4121d78195bdc1569cb
AYUSHI ARYA <83390652+ayushiarya-00@users.noreply.github.com>
2023-01-12 12:18:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ function convertToRoman(num) {
     };
 
   //your code here
+  function convertToRoman(num) {
+  if (num === 0) {
+    return '';
+  }
+  for (var i = 0; i < romanMatrix.length; i++) {
+    if (num >= romanMatrix[i][0]) {
+      return romanMatrix[i][1] + convertToRoman(num - romanMatrix[i][0]);
+    }
+  }
+}
+
+}
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
